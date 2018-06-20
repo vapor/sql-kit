4c0ccf27c3ca8556c843bb827ccb166d2f6ec88a
vkill <vkill.net@gmail.com>
2018-06-20 13:24:42 +0800

    Add rawString to SQLLiteral
    The sql `INSERT INTO "table" ("column", "updatedAt") VALUES ($1, $2) ON CONFLICT ("column") DO UPDATE SET "updatedAt" = EXCLUDED."updatedAt"`
    
    In this case, should use numeric, but numeric is inaccurate.
    
    ```
    .numeric("""
    EXCLUDED."cancellationDate"
    """)
    ```

--- Sources/SQL/SQLLiteral.swift
+++ Sources/SQL/SQLLiteral.swift
@@ -2,11 +2,12 @@ public protocol SQLLiteral: SQLSerializable {
     associatedtype DefaultLiteral: SQLDefaultLiteral
     associatedtype BoolLiteral: SQLBoolLiteral
     static func string(_ string: String) -> Self
+    static func rawString(_ string: String) -> Self
     static func numeric(_ string: String) -> Self
     static var null: Self { get }
     static func `default`(_ default: DefaultLiteral) -> Self
     static func boolean(_ bool: BoolLiteral) -> Self
-    
+
     var isNull: Bool { get }
 }
 
@@ -34,22 +35,27 @@ public enum GenericSQLLiteral<DefaultLiteral, BoolLiteral>: SQLLiteral, Expressi
     public static func string(_ string: String) -> GenericSQLLiteral {
         return ._string(string)
     }
-    
+
+    /// See `SQLLiteral`.
+    public static func rawString(_ string: String) -> GenericSQLLiteral {
+        return ._rawString(string)
+    }
+
     /// See `SQLLiteral`.
     public static func numeric(_ string: String) -> GenericSQLLiteral {
         return ._numeric(string)
     }
-    
+
     /// See `SQLLiteral`.
     public static var null: GenericSQLLiteral {
         return ._null
     }
-    
+
     /// See `SQLLiteral`.
     public static func `default`(_ default: DefaultLiteral) -> GenericSQLLiteral {
         return ._default(`default`)
     }
-    
+
     /// See `SQLLiteral`.
     public static func boolean(_ bool: BoolLiteral) -> GenericSQLLiteral {
         return ._boolean(bool)
@@ -71,11 +77,12 @@ public enum GenericSQLLiteral<DefaultLiteral, BoolLiteral>: SQLLiteral, Expressi
     }
 
     case _string(String)
+    case _rawString(String)
     case _numeric(String)
     case _null
     case _default(DefaultLiteral)
     case _boolean(BoolLiteral)
-    
+
     /// See `SQLLiteral`.
     public var isNull: Bool {
         switch self {
@@ -83,7 +90,7 @@ public enum GenericSQLLiteral<DefaultLiteral, BoolLiteral>: SQLLiteral, Expressi
         default: return false
         }
     }
-    
+
     /// See `SQLSerializable`.
     public func serialize(_ binds: inout [Encodable]) -> String {
         switch self {
@@ -92,6 +99,7 @@ public enum GenericSQLLiteral<DefaultLiteral, BoolLiteral>: SQLLiteral, Expressi
         case ._default(let d): return d.serialize(&binds)
         case ._numeric(let string): return string
         case ._string(let string): return "'" + string + "'"
+        case ._rawString(let string): return string
         }
     }
 }
