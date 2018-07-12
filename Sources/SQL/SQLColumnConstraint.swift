cd72ee998746b43b739060b4bb7d574e91e88a50
tanner0101 <me@tanner.xyz>
2018-07-12 14:55:32 -0400

    add column constraint doc blocks

--- Sources/SQL/SQLColumnConstraint.swift
+++ Sources/SQL/SQLColumnConstraint.swift
@@ -1,12 +1,22 @@
+/// SQL column data integrity constraint, i.e., `NOT NULL`, `PRIMARY KEY`, etc.
+///
+/// Used by `SQLColumnBuilder`.
 public protocol SQLColumnConstraint: SQLSerializable {
+    /// See `SQLIdentifier`.
     associatedtype Identifier: SQLIdentifier
+    
+    /// See `SQLColumnConstraintAlgorithm`.
     associatedtype Algorithm: SQLColumnConstraintAlgorithm
+    
+    /// Creates a new `SQLColumnConstraint` from desired algorithm and identifier.
     static func constraint(_ algorithm: Algorithm, _ identifier: Identifier?) -> Self
 }
 
 // MARK: Convenience
 
 extension SQLColumnConstraint {
+    /// Creates a new `PRIMARY KEY` column constraint with default `DEFAULT` settings
+    /// and no constraint name.
     public static var primaryKey: Self {
         return .primaryKey(default: .default, identifier: nil)
     }
@@ -14,12 +24,14 @@ extension SQLColumnConstraint {
     /// Creates a new `PRIMARY KEY` column constraint.
     ///
     /// - parameters:
+    ///     - default: Optional default value to use.
     ///     - identifier: Optional constraint name.
     /// - returns: New column constraint.
     public static func primaryKey(default: Algorithm.PrimaryKeyDefault? = nil, identifier: Identifier? = nil) -> Self {
         return .constraint(.primaryKey(`default`), identifier)
     }
     
+    /// Creates a new `NOT NULL` column constraint with no constraint name.
     public static var notNull: Self {
         return .notNull(identifier: nil)
     }
@@ -58,12 +70,22 @@ extension SQLColumnConstraint {
     /// Creates a new `CHECK` column constraint.
     ///
     /// - Parameters:
-    ///   - expression: Expression to evaluate when setting the constraint.
+    ///     - expression: Expression to evaluate when setting the constraint.
     /// - Returns: New column constraint.
     public static func check(_ expression: Algorithm.Expression) -> Self {
         return .constraint(.check(expression), nil)
     }
 
+    /// Creates a new `REFERENCES` column constraint.
+    ///
+    ///     .references(\Galaxy.id, onDelete: .delete)
+    ///
+    /// - Parameters:
+    ///     - keyPath: Key path to referenced column.
+    ///     - onDelete: Optional foreign key action to perform on delete.
+    ///     - onUpdate: Optional foreign key action to perform on update.
+    ///     - identifier: Optional constraint name.
+    /// - Returns: New column constraint.
     public static func references<T, V>(
         _ keyPath: KeyPath<T, V>,
         onDelete: Algorithm.ForeignKey.Action? = nil,
@@ -75,6 +97,17 @@ extension SQLColumnConstraint {
         return references(.keyPath(keyPath), [.keyPath(keyPath)], onDelete: onDelete, onUpdate: onUpdate, identifier: identifier)
     }
     
+    /// Creates a new `REFERENCES` column constraint.
+    ///
+    ///     .references("galaxy", ["id"], onDelete: .delete)
+    ///
+    /// - Parameters:
+    ///     - foreignTable: Identifier of foreign table to reference.
+    ///     - foreignColumns: One or more columns in foreign table to reference.
+    ///     - onDelete: Optional foreign key action to perform on delete.
+    ///     - onUpdate: Optional foreign key action to perform on update.
+    ///     - identifier: Optional constraint name.
+    /// - Returns: New column constraint.
     public static func references(
         _ foreignTable: Algorithm.ForeignKey.TableIdentifier,
         _ foreignColumns: [Algorithm.ForeignKey.Identifier],
@@ -89,9 +122,11 @@ extension SQLColumnConstraint {
 
 // MARK: Generic
 
+/// Generic implementation of `SQLColumnConstraint`.
 public struct GenericSQLColumnConstraint<Identifier, Algorithm>: SQLColumnConstraint
     where Identifier: SQLIdentifier, Algorithm: SQLColumnConstraintAlgorithm
 {
+    /// Convenience alias for self.
     public typealias `Self` = GenericSQLColumnConstraint<Identifier, Algorithm>
     
     /// See `SQLColumnConstraint`.
@@ -99,8 +134,10 @@ public struct GenericSQLColumnConstraint<Identifier, Algorithm>: SQLColumnConstr
         return .init(identifier: identifier, algorithm: algorithm)
     }
     
+    /// See `SQLColumnConstraint`.
     public var identifier: Identifier?
     
+    /// See `SQLColumnConstraint`.
     public var algorithm: Algorithm
     
     /// See `SQLSerializable`.
