f8e633f30ebaabb03ac421d6856f56ae3a9f8776
tanner0101 <me@tanner.xyz>
2018-09-20 13:54:55 +0200

    remove other crashing extension in exports.swift file

--- Sources/SQL/Exports.swift
+++ Sources/SQL/Exports.swift
@@ -1,14 +1,3 @@
 @_exported import Async
 @_exported import Core
 @_exported import DatabaseKit
-
-extension SQLSelectBuilder {
-    /// Deprecated.
-    @available(*, deprecated, renamed: "column(_:as:)")
-    public func column(
-        expression: Connection.Query.Expression,
-        as alias: Connection.Query.Identifier? = nil
-    ) -> Self {
-        return column(.expression(expression, alias: alias))
-    }
-}
