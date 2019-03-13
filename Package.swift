7406299af4aaeb79e781581513cb5f2b86290e3d
Tanner Nelson <me@tanner.xyz>
2019-03-13 18:57:29 -0400

    nio 2 convergence

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SQLKitBenchmark", targets: ["SQLKitBenchmark"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", .branch("master"))
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0-convergence"),
     ],
     targets: [
         .target(name: "SQLKit", dependencies: ["NIO"]),
