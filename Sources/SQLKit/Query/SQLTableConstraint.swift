fe3584b4885c09f16ff1f5e6b7d0a21e1b86ce49
Mathew Polzin <matt.polzin@gmail.com>
2019-10-15 22:21:02 -0700

    Open up SQLTableConstraint to make it possible to compose with the similar but not quite already compatible syntax of column foreign key constraints

--- Sources/SQLKit/Query/SQLTableConstraint.swift
+++ Sources/SQLKit/Query/SQLTableConstraint.swift
@@ -1,13 +1,17 @@
 public struct SQLTableConstraint: SQLExpression {
-    public var columns: [SQLExpression]
+    /// Serialized after the name and algorithm but before the modifier
+    public var columns: [SQLExpression]?
     public var algorithm: SQLExpression
     public var name: SQLExpression?
+    /// The last thing serialized in the constraint expression
+    public var modifier: SQLExpression?
     
-    /// Creates a new `SQLColumnConstraint` from desired algorithm and identifier.
-    public init(columns: [SQLExpression], algorithm: SQLExpression, name: SQLExpression?) {
+    /// Creates a new `SQLTableConstraint` from desired algorithm and identifier.
+    public init(columns: [SQLExpression]?, algorithm: SQLExpression, name: SQLExpression?, modifier: SQLExpression? = nil) {
         self.columns = columns
         self.algorithm = algorithm
         self.name = name
+        self.modifier = modifier
     }
     
     public func serialize(to serializer: inout SQLSerializer) {
@@ -17,7 +21,13 @@ public struct SQLTableConstraint: SQLExpression {
             serializer.write(" ")
         }
         self.algorithm.serialize(to: &serializer)
-        serializer.write(" ")
-        SQLGroupExpression(self.columns).serialize(to: &serializer)
+        if let columns = self.columns {
+            serializer.write(" ")
+            SQLGroupExpression(columns).serialize(to: &serializer)
+        }
+        if let modifier = self.modifier {
+            serializer.write(" ")
+            modifier.serialize(to: &serializer)
+        }
     }
 }
