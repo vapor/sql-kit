d09b5527fb0c341f6993e4f5233fadbb7dee1c76
Tanner <tannernelson@gmail.com>
2019-12-13 17:01:47 -0500

    model coding (#79)
    * add SQLRow decoding support
    
    * change spelling to model:, add update builder helper
    
    * row coder updates
    
    * fix tests

--- Sources/SQLKit/SQLRow.swift
+++ Sources/SQLKit/SQLRow.swift
@@ -1,4 +1,15 @@
 public protocol SQLRow {
+    var allColumns: [String] { get }
+    func contains(column: String) -> Bool
+    func decodeNil(column: String) throws -> Bool
     func decode<D>(column: String, as type: D.Type) throws -> D
         where D: Decodable
+}
+
+extension SQLRow {
+    public func decode<D>(model type: D.Type, prefix: String? = nil) throws -> D
+        where D: Decodable
+    {
+        try SQLRowDecoder().decode(D.self, from: self, prefix: prefix)
+    }
 }
