ff0fbee6979db7e426845f757f5efa0725c4b67d
Gwynne Raskind <gwynne@darkrainfall.org>
2020-06-18 13:19:40 -0500

    Add `SQLDropIndexBuilder` counterpart for `SQLDropIndex`. Include it in tests.

---
+++ Sources/SQLKit/Builders/SQLDropIndexBuilder.swift
@@ -0,0 +1,72 @@
+/// Builds `SQLDropIndex` queries.
+///
+///     db.drop(index: "planet_name_unique").run()
+///
+/// See `SQLDropIndex`.
+public final class SQLDropIndexBuilder: SQLQueryBuilder {
+    /// `DropIndex` query being built.
+    public var dropIndex: SQLDropIndex
+    
+    /// See `SQLQueryBuilder`.
+    public var database: SQLDatabase
+    
+    /// See `SQLQueryBuilder`.
+    public var query: SQLExpression {
+        return self.dropIndex
+    }
+    
+    /// Creates a new `SQLDropIndexBuilder`.
+    public init(_ dropIndex: SQLDropIndex, on database: SQLDatabase) {
+        self.dropIndex = dropIndex
+        self.database = database
+    }
+
+    /// The optional `IF EXISTS` clause suppresses the error that would normally
+    /// result if the index does not exist.
+    public func ifExists() -> Self {
+        dropIndex.ifExists = true
+        return self
+    }
+
+    /// The drop behavior clause specifies if objects that depend on a index
+    /// should also be dropped or not when the index is dropped, for databases
+    /// that support this.
+    public func behavior(_ behavior: SQLDropBehavior) -> Self {
+        dropIndex.behavior = behavior
+        return self
+    }
+
+    /// Adds a `CASCADE` clause to the `DROP INDEX` statement instructing that
+    /// objects that depend on this index should also be dropped.
+    public func cascade() -> Self {
+        dropIndex.behavior = SQLDropBehavior.cascade
+        return self
+    }
+
+    /// Adds a `RESTRICT` clause to the `DROP INDEX` statement instructing that
+    /// if any objects depend on this index, the drop should be refused.
+    public func restrict() -> Self {
+        dropIndex.behavior = SQLDropBehavior.restrict
+        return self
+    }
+}
+
+// MARK: Connection
+
+extension SQLDatabase {
+    /// Creates a new `SQLDropIndexBuilder`.
+    ///
+    ///     db.drop(index: "foo").run()
+    ///
+    public func drop(index name: String) -> SQLDropIndexBuilder {
+        return self.drop(index: SQLIdentifier(name))
+    }
+    
+    /// Creates a new `SQLDropIndexBuilder`.
+    ///
+    ///     db.drop(index: "foo").run()
+    ///
+    public func drop(index name: SQLExpression) -> SQLDropIndexBuilder {
+        return .init(.init(name: name), on: self)
+    }
+}
