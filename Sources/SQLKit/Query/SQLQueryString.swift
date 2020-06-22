d044d36b1179b72f7c24fb756979c88cb767fcb4
Gwynne Raskind <gwynne@darkrainfall.org>
2020-06-22 15:23:55 -0500

    Add additional interpolations, constructors, and operations to SQLQueryString (#112)
    * Replace `SQLQueryString`'s `Fragment` enum with saving generic `SQLExpression`s as the string fragments, using `SQLRaw`, `SQLList`, and `SQLBind` to support the existing interpolation kinds.
    * Add comments to all `SQLQueryString` methods to help clarify usage and behavior.
    * Add integer literal, boolean literal, string literal, list of string literal, SQL identifier, list of SQL identifier, and raw SQL expression interpolations to `SQLQueryString`.
    * Move the `SQLQueryString` interpolation tests to their own `XCTestCase` and add a new test covering each available type of interpolation currently implemented.
    * Add operator `+` for concatenating two `SQLQueryString`s. Credit to @t-ae for the idea and original implementation.
    * Implement `Array<SQLQueryString>.joined(separator:)`. Credit to @t-ae for the idea and original implementation.
    * Improve a check in a query string test. Credit to @t-ae for finding the issue.

--- Sources/SQLKit/Query/SQLQueryString.swift
+++ Sources/SQLKit/Query/SQLQueryString.swift
@@ -1,65 +1,130 @@
 public struct SQLQueryString {
-    enum Fragment {
-        case literal(String)
-        case value(Encodable)
-        case values([Encodable])
-    }
+    var fragments: [SQLExpression]
     
-    var fragments: [Fragment]
-    
+    /// Create a query string from a plain string containing raw SQL.
     public init<S: StringProtocol>(_ string: S) {
-        fragments = [.literal(string.description)]
+        self.fragments = [SQLRaw(string.description)]
     }
 }
 
 extension SQLQueryString: ExpressibleByStringLiteral {
+    /// See `ExpressibleByStringLiteral.init(stringLiteral:)`
     public init(stringLiteral value: String) {
-        fragments = [.literal(value)]
+        self.init(value)
     }
 }
 
 extension SQLQueryString: ExpressibleByStringInterpolation {
-    
+    /// See `ExpressibleByStringInterpolation.init(stringInterpolation:)`
     public init(stringInterpolation: SQLQueryString) {
-        fragments = stringInterpolation.fragments
+        self.fragments = stringInterpolation.fragments
     }
 }
 
 extension SQLQueryString: StringInterpolationProtocol {
+    /// See `StringInterpolationProtocol.init(literalCapacity:interpolationCount:)`
     public init(literalCapacity: Int, interpolationCount: Int) {
-        fragments = []
+        self.fragments = []
     }
     
+    /// Adds raw SQL to the string. Despite the use of the term "literal" dictated by the interpolation protocol, this
+    /// produces `SQLRaw` content, _not_ SQL string literals.
     mutating public func appendLiteral(_ literal: String) {
-        fragments.append(.literal(literal))
+        self.fragments.append(SQLRaw(literal))
     }
     
+    /// Adds an interpolated string of raw SQL. Despite the use of the term "literal" dictated by the interpolation
+    /// protocol, this produces `SQLRaw` content, _not_ SQL string literals.
     mutating public func appendInterpolation(_ literal: String) {
-        fragments.append(.literal(literal))
+        self.fragments.append(SQLRaw(literal))
     }
-
+    
+    /// Embed an `Encodable` value as a binding in the SQL query.
     mutating public func appendInterpolation(bind value: Encodable) {
-        fragments.append(.value(value))
+        self.fragments.append(SQLBind(value))
     }
 
-    /// Binds multiple values in a comma separated list.
-    /// Commonly used with the `IN` operator.
+    /// Embed multiple `Encodable` values as bindings in the SQL query, separating the bind placeholders with commas.
+    /// Most commonly useful when working with the `IN` operator.
     mutating public func appendInterpolation(binds values: [Encodable]) {
-        fragments.append(.values(values))
+        self.fragments.append(SQLList(values.map(SQLBind.init)))
+    }
+    
+    /// Embed an integer as a literal value, as if via `SQLLiteral.numeric()`
+    /// Use this preferentially to ensure values are appropriately represented in the database's dialect.
+    mutating public func appendInterpolation<I: BinaryInteger>(literal: I) {
+        self.fragments.append(SQLLiteral.numeric("\(literal)"))
+    }
+
+    /// Embed a `Bool` as a literal value, as if via `SQLLiteral.boolean()`
+    mutating public func appendInterpolation(_ value: Bool) {
+        self.fragments.append(SQLLiteral.boolean(value))
+    }
+
+    /// Embed a `String` as a literal value, as if via `SQLLiteral.string()`
+    /// Use this preferentially to ensure string values are appropriately represented in the database's dialect.
+    mutating public func appendInterpolation(literal: String) {
+        self.fragments.append(SQLLiteral.string(literal))
+    }
+
+    /// Embed an array of `Strings` as a list of literal values, using the `joiner` to separate them.
+    ///
+    /// Example:
+    ///
+    ///     "SELECT \(literals: "a", "b", "c", "d", joinedBy: "||") FROM nowhere"
+    ///
+    /// Rendered by the SQLite dialect:
+    ///
+    ///     SELECT 'a'||'b'||'c'||'d' FROM nowhere
+    mutating public func appendInterpolation(literals: [String], joinedBy joiner: String) {
+        self.fragments.append(SQLList(literals.map(SQLLiteral.string(_:)), separator: SQLRaw(joiner)))
+    }
+
+    /// Embed a `String` as an SQL identifier, as if with `SQLIdentifier`
+    /// Use this preferentially to ensure table names, column names, and other non-keyword identifiers are appropriately
+    /// represented in the database's dialect.
+    mutating public func appendInterpolation(ident: String) {
+        self.fragments.append(SQLIdentifier(ident))
+    }
+
+    /// Embed an array of `Strings` as a list of SQL identifiers, using the `joiner` to separate them.
+    ///
+    /// - Important: This interprets each string as an identifier, _not_ as a literal value!
+    ///
+    /// Example:
+    ///
+    ///     "SELECT \(idents: "a", "b", "c", "d", joinedBy: ",") FROM \(ident: "nowhere")"
+    ///
+    /// Rendered by the SQLite dialect:
+    ///
+    ///     SELECT "a", "b", "c", "d" FROM "nowhere"
+    mutating public func appendInterpolation(idents: [String], joinedBy joiner: String) {
+        self.fragments.append(SQLList(idents.map(SQLIdentifier.init(_:)), separator: SQLRaw(joiner)))
+    }
+
+    /// Embed any `SQLExpression` into the string, to be serialized according to its type.
+    mutating public func appendInterpolation(_ expression: SQLExpression) {
+        self.fragments.append(expression)
+    }
+}
+
+extension SQLQueryString {
+    public static func +(lhs: SQLQueryString, rhs: SQLQueryString) -> SQLQueryString {
+        return "\(lhs)\(rhs)"
+    }
+}
+
+extension Array where Element == SQLQueryString {
+    public func joined(separator: String) -> SQLQueryString {
+        let separator = "\(separator)" as SQLQueryString
+        
+        return self.first.map { self.dropFirst().lazy.reduce($0) { $0 + separator + $1 } } ?? ""
     }
 }
 
 extension SQLQueryString: SQLExpression {
+    /// See `SQLExpression.serialize(to:)`
     public func serialize(to serializer: inout SQLSerializer) {
-        for fragment in fragments {
-            switch fragment {
-            case let .literal(str):
-                serializer.write(str)
-            case let .value(v):
-                serializer.write(bind: v)
-            case let .values(l):
-                SQLList(l.map { SQLBind($0) }).serialize(to: &serializer)
-            }
-        }
+        self.fragments.forEach { $0.serialize(to: &serializer) }
     }
 }
---
+++ Tests/SQLKitTests/SQLQueryStringTests.swift
@@ -0,0 +1,98 @@
+import SQLKit
+import SQLKitBenchmark
+import XCTest
+
+final class SQLQueryStringTests: XCTestCase {
+    var db: TestDatabase!
+
+    override func setUp() {
+        super.setUp()
+        self.db = TestDatabase()
+    }
+
+    func testRawQueryStringInterpolation() throws {
+        let (table, planet) = ("planets", "Earth")
+        let builder = db.raw("SELECT * FROM \(table) WHERE name = \(bind: planet)")
+        var serializer = SQLSerializer(database: db)
+        builder.query.serialize(to: &serializer)
+
+        XCTAssertEqual(serializer.sql, "SELECT * FROM planets WHERE name = ?")
+        XCTAssertEqual(serializer.binds.first! as! String, planet)
+    }
+    
+    func testRawQueryStringWithNonliteral() throws {
+        let (table, planet) = ("planets", "Earth")
+
+        var serializer1 = SQLSerializer(database: db)
+        let query1 = "SELECT * FROM \(table) WHERE name = \(planet)"
+        let builder1 = db.raw(.init(query1))
+        builder1.query.serialize(to: &serializer1)
+        XCTAssertEqual(serializer1.sql, "SELECT * FROM planets WHERE name = Earth")
+
+        var serializer2 = SQLSerializer(database: db)
+        let query2: Substring = "|||SELECT * FROM staticTable WHERE name = uselessUnboundValue|||".dropFirst(3).dropLast(3)
+        let builder2 = db.raw(.init(query2))
+        builder2.query.serialize(to: &serializer2)
+        XCTAssertEqual(serializer2.sql, "SELECT * FROM staticTable WHERE name = uselessUnboundValue")
+    }
+
+    func testMakeQueryStringWithoutRawBuilder() throws {
+        let queryString = SQLQueryString("query with \(ident: "identifier") and stuff")
+        var serializer = SQLSerializer(database: db)
+        let builder = db.raw(queryString)
+        builder.query.serialize(to: &serializer)
+        XCTAssertEqual(serializer.sql, "query with `identifier` and stuff")
+    }
+    
+    func testAllQueryStringInterpolationTypes() throws {
+        var serializer = SQLSerializer(database: db)
+        let builder = db.raw("""
+            Query string embeds:
+                \("plain string embed")
+                \(bind: "single bind embed")
+                \(binds: ["multi-bind embed one", "multi-bind embed two"])
+                numeric literal embed \(literal: 1)
+                boolean literal embeds \(true) and \(false)
+                \(literal: "string literal embed")
+                \(literals: ["multi-literal embed one", "multi-literal embed two"], joinedBy: " || ")
+                \(ident: "string identifier embed")
+                \(idents: ["multi-ident embed one", "multi-ident embed two"], joinedBy: " + ")
+                expression embeds: \(SQLDropBehavior.restrict) and \(SQLDropBehavior.cascade)
+            """)
+        builder.query.serialize(to: &serializer)
+        XCTAssertEqual(serializer.sql, """
+            Query string embeds:
+                plain string embed
+                ?
+                ?, ?
+                numeric literal embed 1
+                boolean literal embeds true and false
+                'string literal embed'
+                'multi-literal embed one' || 'multi-literal embed two'
+                `string identifier embed`
+                `multi-ident embed one` + `multi-ident embed two`
+                expression embeds: RESTRICT and CASCADE
+            """)
+    }
+    
+    func testAppendingQueryStringByOperatorPlus() throws {
+        var serializer = SQLSerializer(database: db)
+        let builder = db.raw(
+            "INSERT INTO \(ident: "anything") " as SQLQueryString +
+            "(\(idents: ["col1", "col2", "col3"], joinedBy: ",")) " as SQLQueryString +
+            "VALUES (\(binds: [1, 2, 3]))" as SQLQueryString
+        )
+        builder.query.serialize(to: &serializer)
+        XCTAssertEqual(serializer.sql, "INSERT INTO `anything` (`col1`,`col2`,`col3`) VALUES (?, ?, ?)")
+    }
+    
+    func testQueryStringArrayJoin() throws {
+        var serializer = SQLSerializer(database: db)
+        let builder = db.raw(
+            "INSERT INTO \(ident: "anything") " as SQLQueryString +
+            ((0..<5).map { "\(literal: "\($0)")" as SQLQueryString }).joined(separator: "..")
+        )
+        builder.query.serialize(to: &serializer)
+        XCTAssertEqual(serializer.sql, "INSERT INTO `anything` '0'..'1'..'2'..'3'..'4'")
+    }
+}
