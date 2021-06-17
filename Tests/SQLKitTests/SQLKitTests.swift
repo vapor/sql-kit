7c1227f18a8e493559f05197c02d8dd0608c44bc
Sven A. Schmidt <sas@finestructure.co>
2021-06-17 07:34:11 +0200

    Test more than a binary expression

--- Tests/SQLKitTests/SQLKitTests.swift
+++ Tests/SQLKitTests/SQLKitTests.swift
@@ -724,15 +724,20 @@ CREATE TABLE `planets`(`id` BIGINT, `name` TEXT, `diameter` INTEGER, `galaxy_nam
                 .column("id")
                 .from("t1")
                 .where("f1", .equal, "foo")
-                .limit(2),
+                .limit(1),
             db.select()
                 .column("id")
                 .from("t2")
                 .where("f2", .equal, "bar")
+                .limit(2),
+            db.select()
+                .column("id")
+                .from("t3")
+                .where("f3", .equal, "baz")
                 .limit(3)
         ).run().wait()
 
-        XCTAssertEqual(db.results[0], "(SELECT `id` FROM `t1` WHERE `f1` = ? LIMIT 2) UNION (SELECT `id` FROM `t2` WHERE `f2` = ? LIMIT 3)")
+        XCTAssertEqual(db.results[0], "(SELECT `id` FROM `t1` WHERE `f1` = ? LIMIT 1) UNION (SELECT `id` FROM `t2` WHERE `f2` = ? LIMIT 2) UNION (SELECT `id` FROM `t3` WHERE `f3` = ? LIMIT 3)")
     }
 
 }
