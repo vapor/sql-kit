a230eebd036e26ac1124211182456e40752a183f
Sven A. Schmidt <sas@finestructure.co>
2021-06-17 07:37:38 +0200

    Fix `error: variable used within its own initial value` on some CI platforms

--- Sources/SQLKit/Query/SQLUnion.swift
+++ Sources/SQLKit/Query/SQLUnion.swift
@@ -10,10 +10,10 @@ public struct SQLUnion: SQLExpression {
     }
 
     public func serialize(to serializer: inout SQLSerializer) {
-        let args = args.map(SQLGroupExpression.init)
-        guard let first = args.first else { return }
+        let groups = args.map(SQLGroupExpression.init)
+        guard let first = groups.first else { return }
         first.serialize(to: &serializer)
-        for arg in args.dropFirst() {
+        for arg in groups.dropFirst() {
             serializer.write(" UNION ")
             arg.serialize(to: &serializer)
         }
