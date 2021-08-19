d0124aa593bea3ff1eeed9b69d0d8d911e79be40
Sven A. Schmidt <sas@finestructure.co>
2021-08-19 07:09:14 +0200

    Update Sources/SQLKit/Query/SQLUnion.swift
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>

--- Sources/SQLKit/Query/SQLUnion.swift
+++ Sources/SQLKit/Query/SQLUnion.swift
@@ -7,18 +7,24 @@ public struct SQLUnion: SQLExpression {
     }
 
     public init(_ args: [SQLExpression], all: Bool = false) {
+        precondition(!args.isEmpty, "Empty SQLUnions are not valid.") 
         self.args = args
         self.all = all
     }
 
     public func serialize(to serializer: inout SQLSerializer) {
-        let groups = args.map(SQLGroupExpression.init)
-        guard let first = groups.first else { return }
-        first.serialize(to: &serializer)
-        for arg in groups.dropFirst() {
-            serializer.write(all ? " UNION ALL " : " UNION ")
-            arg.serialize(to: &serializer)
-        }
+        self.args
+            .map { [SQLGroupExpression($0)] }
+            .joined([SQLUnionJoiner(all: self.all)])
+            .forEach { $0.serialize(to: &serializer) }
+    }
+}
+
+fileprivate struct SQLUnionJoiner: SQLExpression {
+    let all: Bool
+    
+    func serialize(to serializer: inout SQLSerializer) {
+        serializer.write("UNION\(self.all ? " ALL" : "") ")
     }
 }
 
