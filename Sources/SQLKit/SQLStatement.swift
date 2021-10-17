81380e8db73168f16217ad0b26e934c3dd2a980e
Gwynne Raskind <gwynne@darkrainfall.org>
2021-10-17 07:27:28 -0500

    Add doc comments and an extra utility method to SQLStatement

--- Sources/SQLKit/SQLStatement.swift
+++ Sources/SQLKit/SQLStatement.swift
@@ -6,28 +6,44 @@ extension SQLSerializer {
     }
 }
 
+/// A helper type for building complete SQL statements up from fragments.
+/// See also `SQLSerializer.statement(_:)`.
 public struct SQLStatement: SQLExpression {
     public var parts: [SQLExpression]
     let database: SQLDatabase
 
+    /// Convenience accessor for the database's `SQLDialect`.
+    ///
+    /// - Note: Statement closures can't access the serializer directly due to exclusive access rules.
     public var dialect: SQLDialect {
         self.database.dialect
     }
 
+    /// Add raw text to the output.
     public mutating func append(_ raw: String) {
         self.append(SQLRaw(raw))
     }
+    
+    /// Add raw text followed by an `SQLExpression` to tbe output.
+    ///
+    /// - Note: "Text + expr" pairs appear quite often when building statments.
+    public mutating func append(_ raw: String, _ part: SQLExpression) {
+        self.append(raw)
+        self.append(part)
+    }
 
+    /// Add an `SQLExpression` of any kind to the output.
     public mutating func append(_ part: SQLExpression) {
         self.parts.append(part)
     }
 
+    /// See `SQLSerializer.serialize(to:)`.
     public func serialize(to serializer: inout SQLSerializer) {
-        for (i, part) in parts.enumerated() {
-            if i != 0 {
+        for i in self.parts.indices {
+            if i > self.parts.startIndex {
                 serializer.write(" ")
             }
-            part.serialize(to: &serializer)
+            self.parts[i].serialize(to: &serializer)
         }
     }
 }
