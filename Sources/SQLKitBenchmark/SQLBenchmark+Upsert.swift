71b6deac29f18fe3e05964741d3f88995e9fe360
Gwynne Raskind <gwynne@darkrainfall.org>
2021-10-22 08:25:05 -0500

    MySQL 5.7 and MariaDB can't handle modern Unicode. Everyone else has no problem, but MySQL messes it up. This is why we can't have nice things.

--- Sources/SQLKitBenchmark/SQLBenchmark+Upsert.swift
+++ Sources/SQLKitBenchmark/SQLBenchmark+Upsert.swift
@@ -60,12 +60,12 @@ extension SQLBenchmarker {
         try self.database.create(table: Self.testSchema).column(definitions: Self.testColDefs).unique(["planet_id", "point_of_origin"]).run().wait()
         try self.database.insert(into: Self.testSchema)
             .columns(Self.testCols)
-            .values(Self.testVals(planet: 1, poi: "𑀙", setting: 299_792_458.0/*𝑐*/, start: Date() - (31_557_600 * 1.8)))
-            .values(Self.testVals(planet: 1, poi: "𑀵", setting: nil, start: Date() - 8_640_000.0, finish: Date.distantFuture))
-            .values(Self.testVals(planet: 2, poi: "ᛡ", setting: 3.141592653589793238/*π*/, start: Date() - 31_557_600_000.0))
-            .values(Self.testVals(planet: 3, poi: "𐋈", setting: 2.7182818284/*𝑒*/, start: Date.distantPast))
-            .values(Self.testVals(planet: 4, poi: "𑁍", setting: 1.4142135623/*√2*/, start: Date())) // Date.bigBang sadly isn't a thing
-            .values(Self.testVals(planet: 4, poi: "𑁞", setting: 6.62607015/*𝒉×10³⁴*/, start: Date(timeIntervalSinceReferenceDate: Date.timeIntervalSinceReferenceDate.nextUp)))
+            .values(Self.testVals(planet: 1, poi: "A", setting: 299_792_458.0/*𝑐*/, start: Date() - (31_557_600 * 1.8)))
+            .values(Self.testVals(planet: 1, poi: "B", setting: nil, start: Date() - 8_640_000.0, finish: Date.distantFuture))
+            .values(Self.testVals(planet: 2, poi: "C", setting: 3.141592653589793238/*π*/, start: Date() - 31_557_600_000.0))
+            .values(Self.testVals(planet: 3, poi: "D", setting: 2.7182818284/*𝑒*/, start: Date.distantPast))
+            .values(Self.testVals(planet: 4, poi: "E", setting: 1.4142135623/*√2*/, start: Date())) // Date.bigBang sadly isn't a thing
+            .values(Self.testVals(planet: 4, poi: "F", setting: 6.62607015/*𝒉×10³⁴*/, start: Date(timeIntervalSinceReferenceDate: Date.timeIntervalSinceReferenceDate.nextUp)))
             .run().wait()
     }
     
@@ -83,8 +83,8 @@ extension SQLBenchmarker {
         testInsert(ok: true, Self.testVals(id: 1, planet: 1, poi: "0")) { $0.onConflict(with: ["id"]) { $0.set("last_status_update", to: Date().timeIntervalSince1970) } }
         XCTAssertEqual(try self.testCount { $0.where("last_status_update", .isNot, SQLLiteral.null) }, 1)
             
-        testInsert(ok: true, Self.testVals(planet: 2, poi: "ᛡ", update: Date())) { $0.onConflict(with: ["planet_id", "point_of_origin"]) { $0.set(excludedValueOf: "last_status_update") } }
-        XCTAssertEqual(try self.testCount { $0.where("planet_id", .equal, 2).where("point_of_origin", .equal, "ᛡ").where("last_status_update", .isNot, SQLLiteral.null) }, 1)
+        testInsert(ok: true, Self.testVals(planet: 2, poi: "C", update: Date())) { $0.onConflict(with: ["planet_id", "point_of_origin"]) { $0.set(excludedValueOf: "last_status_update") } }
+        XCTAssertEqual(try self.testCount { $0.where("planet_id", .equal, 2).where("point_of_origin", .equal, "C").where("last_status_update", .isNot, SQLLiteral.null) }, 1)
             
         /// Lots of other cases need verification - collisions with multiple uniques in the same row and different
         /// rows, updates of multiple rows, etc.
@@ -94,8 +94,8 @@ extension SQLBenchmarker {
     public func testUpserts_predicateUpdate() throws {
         guard self.database.dialect.upsertSyntax != .mysqlLike else { return } // not supported by MySQL syntax
         
-        testInsert(ok: true, Self.testVals(planet: 4, poi: "𑁞", update: Date())) { $0.onConflict(with: ["planet_id", "point_of_origin"]) { $0.set(excludedValueOf: "last_status_update").where(SQLExcludedColumn("last_status_update"), .is, SQLLiteral.null) } }
-        XCTAssertEqual(try self.testCount { $0.where("planet_id", .equal, 4).where("point_of_origin", .equal, "𑁞").where("last_status_update", .isNot, SQLLiteral.null) }, 0)
+        testInsert(ok: true, Self.testVals(planet: 4, poi: "F", update: Date())) { $0.onConflict(with: ["planet_id", "point_of_origin"]) { $0.set(excludedValueOf: "last_status_update").where(SQLExcludedColumn("last_status_update"), .is, SQLLiteral.null) } }
+        XCTAssertEqual(try self.testCount { $0.where("planet_id", .equal, 4).where("point_of_origin", .equal, "F").where("last_status_update", .isNot, SQLLiteral.null) }, 0)
     }
     
     /// Remove tables used by these tests.
