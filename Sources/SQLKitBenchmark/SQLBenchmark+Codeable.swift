13f9d8c92d8bd145c7053a82d67045767b05c7b6
Gwynne Raskind <gwynne@darkrainfall.org>
2021-10-22 08:27:08 -0500

    Since we're here, fix that we didn't use `runTest` in any of the places we meant to.

--- Sources/SQLKitBenchmark/SQLBenchmark+Codeable.swift
+++ Sources/SQLKitBenchmark/SQLBenchmark+Codeable.swift
@@ -2,33 +2,35 @@ import SQLKit
 
 extension SQLBenchmarker {
     public func testCodable() throws {
-        try self.database.drop(table: "planets")
-            .ifExists()
-            .run().wait()
-        try self.database.drop(table: "galaxies")
-            .ifExists()
-            .run().wait()
-        try self.database.create(table: "galaxies")
-            .column("id", type: .bigint, .primaryKey)
-            .column("name", type: .text)
-            .run().wait()
-        try self.database.create(table: "planets")
-            .column("id", type: .bigint, .primaryKey)
-            .column("name", type: .text, [.default(SQLLiteral.string("Unamed Planet")), .notNull])
-            .column("is_inhabited", type: .custom(SQLRaw("boolean")), .notNull)
-            .column("galaxyID", type: .bigint, .references("galaxies", "id"))
-            .run().wait()
+        try self.runTest {
+            try self.database.drop(table: "planets")
+                .ifExists()
+                .run().wait()
+            try self.database.drop(table: "galaxies")
+                .ifExists()
+                .run().wait()
+            try self.database.create(table: "galaxies")
+                .column("id", type: .bigint, .primaryKey)
+                .column("name", type: .text)
+                .run().wait()
+            try self.database.create(table: "planets")
+                .column("id", type: .bigint, .primaryKey)
+                .column("name", type: .text, [.default(SQLLiteral.string("Unamed Planet")), .notNull])
+                .column("is_inhabited", type: .custom(SQLRaw("boolean")), .notNull)
+                .column("galaxyID", type: .bigint, .references("galaxies", "id"))
+                .run().wait()
 
-        // insert
-        let galaxy = Galaxy(name: "milky way")
-        try self.database.insert(into: "galaxies").model(galaxy).run().wait()
+            // insert
+            let galaxy = Galaxy(name: "milky way")
+            try self.database.insert(into: "galaxies").model(galaxy).run().wait()
 
-        // insert with keyEncodingStrategy
-        let earth = Planet(name: "Earth", isInhabited: true)
-        let mars = Planet(name: "Mars", isInhabited: false)
-        try self.database.insert(into: "planets")
-            .models([earth, mars], keyEncodingStrategy: .convertToSnakeCase)
-            .run().wait()
+            // insert with keyEncodingStrategy
+            let earth = Planet(name: "Earth", isInhabited: true)
+            let mars = Planet(name: "Mars", isInhabited: false)
+            try self.database.insert(into: "planets")
+                .models([earth, mars], keyEncodingStrategy: .convertToSnakeCase)
+                .run().wait()
+        }
     }
 }
 
