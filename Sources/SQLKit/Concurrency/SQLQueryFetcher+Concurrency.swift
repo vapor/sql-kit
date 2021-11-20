945ee67d8581618337f2536be73a955e9277ee8e
Gwynne Raskind <gwynne@darkrainfall.org>
2021-11-20 17:24:41 -0600

    Add Concurrency extensions for SQLKit. Not much needed here!

---
+++ Sources/SQLKit/Concurrency/SQLQueryFetcher+Concurrency.swift
@@ -0,0 +1,31 @@
+#if compiler(>=5.5) && canImport(_Concurrency)
+import NIOCore
+
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+public extension SQLQueryFetcher {
+    func first<D>(decoding: D.Type) async throws -> D? where D: Decodable {
+        return try await self.first(decoding: D.self).get()
+    }
+    
+    func first() async throws -> SQLRow? {
+        return try await self.first().get()
+    }
+    
+    func all<D>(decoding: D.Type) async throws -> [D] where D: Decodable {
+        return try await self.all(decoding: D.self).get()
+    }
+    
+    func all() async throws -> [SQLRow] {
+        return try await self.all().get()
+    }
+    
+    func run<D>(decoding: D.Type, _ handler: @escaping (Result<D, Error>) -> ()) async throws -> Void where D: Decodable {
+        return try await self.run(decoding: D.self, handler).get()
+    }
+    
+    func run(_ handler: @escaping (SQLRow) -> ()) async throws -> Void {
+        return try await self.run(handler).get()
+    }
+}
+
+#endif
