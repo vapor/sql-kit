dcf10a00d7d5df987b7948e6fd5596fb65f6d0c2
Maciej Trybilo <maciektr@gmail.com>
2022-11-15 12:47:17 +0100

    Support += operator for SQLQueryString. (#156)
    * Support += operator for SQLQueryString.

--- Sources/SQLKit/Query/SQLQueryString.swift
+++ Sources/SQLKit/Query/SQLQueryString.swift
@@ -119,6 +119,10 @@ extension SQLQueryString {
     public static func +(lhs: SQLQueryString, rhs: SQLQueryString) -> SQLQueryString {
         return "\(lhs)\(rhs)"
     }
+    
+    public static func +=(lhs: inout SQLQueryString, rhs: SQLQueryString) {
+        lhs.fragments.append(contentsOf: rhs.fragments)
+    }
 }
 
 extension Array where Element == SQLQueryString {
