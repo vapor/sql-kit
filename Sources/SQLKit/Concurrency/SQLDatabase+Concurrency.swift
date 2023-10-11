1d055d75d22061ed09afd2c74f2a86fb5f4f5745
Gwynne Raskind <gwynne@vapor.codes>
2023-10-11 05:30:44 -0500

    Improve the behavior of the async-aware implementations

--- Sources/SQLKit/Concurrency/SQLDatabase+Concurrency.swift
+++ Sources/SQLKit/Concurrency/SQLDatabase+Concurrency.swift
@@ -1,7 +1,6 @@
-import NIOCore
-
-public extension SQLDatabase {
-    func execute(sql query: any SQLExpression, _ onRow: @escaping @Sendable (any SQLRow) -> ()) async throws {
+extension SQLDatabase {
+    /// Concurrency-aware version of ``SQLDatabase/execute(sql:_:)-90wi9``.
+    public func execute(sql query: any SQLExpression, _ onRow: @escaping @Sendable (any SQLRow) -> ()) async throws {
         try await self.execute(sql: query, onRow).get()
     }
 }
