c3544717d4fd44d0aae29ee974fb962637aa2e55
Gwynne Raskind <gwynne@vapor.codes>
2023-10-11 05:30:44 -0500

    Improve the behavior of the async-aware implementations

--- Sources/SQLKit/Concurrency/SQLQueryBuilder+Concurrency.swift
+++ Sources/SQLKit/Concurrency/SQLQueryBuilder+Concurrency.swift
@@ -1,7 +1,7 @@
-import NIOCore
-
-public extension SQLQueryBuilder {
-    func run() async throws -> Void {
-        try await self.run().get()
+extension SQLQueryBuilder {
+    /// Concurrency-aware version of ``SQLQueryBuilder/run()-2zws8``.
+    @inlinable
+    public func run() async throws -> Void {
+        try await self.database.execute(sql: self.query) { _ in }
     }
 }
--- Sources/SQLKit/Concurrency/SQLQueryFetcher+Concurrency.swift
+++ Sources/SQLKit/Concurrency/SQLQueryFetcher+Concurrency.swift
@@ -1,27 +1,43 @@
-import NIOCore
-
-public extension SQLQueryFetcher {
-    func first<D>(decoding: D.Type) async throws -> D? where D: Decodable {
-        try await self.first(decoding: D.self).get()
+extension SQLQueryFetcher {
+    /// Concurrency-aware version of ``SQLQueryFetcher/first(decoding:)-6gqh3``.
+    @inlinable
+    public func first<D>(decoding: D.Type) async throws -> D? where D: Decodable {
+        try await self.first()?.decode(model: D.self)
     }
     
-    func first() async throws -> (any SQLRow)? {
-        try await self.first().get()
+    /// Concurrency-aware version of ``SQLQueryFetcher/first()-7o93q``.
+    @inlinable
+    public func first() async throws -> (any SQLRow)? {
+        if let partialBuilder = self as? (any SQLPartialResultBuilder & SQLQueryFetcher) {
+            return try await partialBuilder.limit(1).all().first
+        } else {
+            return try await self.all().first
+        }
     }
     
-    func all<D>(decoding: D.Type) async throws -> [D] where D: Decodable {
-        try await self.all(decoding: D.self).get()
+    /// Concurrency-aware version of ``SQLQueryFetcher/all(decoding:)-6q02f``.
+    @inlinable
+    public func all<D>(decoding: D.Type) async throws -> [D] where D: Decodable {
+        try await self.all().map { try $0.decode(model: D.self) }
     }
     
-    func all() async throws -> [any SQLRow] {
-        try await self.all().get()
+    /// Concurrency-aware version of ``SQLQueryFetcher/all()-5j67e``.
+    @inlinable
+    public func all() async throws -> [any SQLRow] {
+        let rows = RowsBox()
+        try await self.run { rows.all.append($0) }
+        return rows.all
     }
     
-    func run<D>(decoding: D.Type, _ handler: @escaping @Sendable (Result<D, any Error>) -> ()) async throws -> Void where D: Decodable {
-        try await self.run(decoding: D.self, handler).get()
+    /// Concurrency-aware version of ``SQLQueryFetcher/run(decoding:_:)-6z89k``.
+    @inlinable
+    public func run<D>(decoding: D.Type, _ handler: @escaping @Sendable (Result<D, any Error>) -> ()) async throws -> Void where D: Decodable {
+        try await self.run { row in handler(Result { try row.decode(model: D.self) }) }
     }
     
-    func run(_ handler: @escaping @Sendable (any SQLRow) -> ()) async throws -> Void {
-        try await self.run(handler).get()
+    /// Concurrency-aware version of ``SQLQueryFetcher/run(_:)-542bs``.
+    @inlinable
+    public func run(_ handler: @escaping @Sendable (any SQLRow) -> ()) async throws -> Void {
+        try await self.database.execute(sql: self.query, handler)
     }
 }
