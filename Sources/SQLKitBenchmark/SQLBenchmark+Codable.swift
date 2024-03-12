0730b738cfc992967a620f6a7af48ee15cf40a36
Gwynne Raskind <gwynne@vapor.codes>
2024-03-11 19:19:21 -0500

    Make SQLBenchmarker async

--- Sources/SQLKitBenchmark/SQLBenchmark+Codable.swift
+++ Sources/SQLKitBenchmark/SQLBenchmark+Codable.swift
@@ -1,35 +1,35 @@
 import SQLKit
 
 extension SQLBenchmarker {
-    public func testCodable() throws {
-        try self.runTest {
-            try $0.drop(table: "planets")
+    public func testCodable() async throws {
+        try await self.runTest {
+            try await $0.drop(table: "planets")
                 .ifExists()
-                .run().wait()
-            try $0.drop(table: "galaxies")
+                .run()
+            try await $0.drop(table: "galaxies")
                 .ifExists()
-                .run().wait()
-            try $0.create(table: "galaxies")
+                .run()
+            try await $0.create(table: "galaxies")
                 .column("id", type: .bigint, .primaryKey)
                 .column("name", type: .text)
-                .run().wait()
-            try $0.create(table: "planets")
+                .run()
+            try await $0.create(table: "planets")
                 .column("id", type: .bigint, .primaryKey)
                 .column("name", type: .text, [.default(SQLLiteral.string("Unamed Planet")), .notNull])
                 .column("is_inhabited", type: .custom(SQLRaw("boolean")), .notNull)
                 .column("galaxyID", type: .bigint, .references("galaxies", "id"))
-                .run().wait()
+                .run()
 
             // insert
             let galaxy = Galaxy(name: "milky way")
-            try $0.insert(into: "galaxies").model(galaxy).run().wait()
+            try await $0.insert(into: "galaxies").model(galaxy).run()
 
             // insert with keyEncodingStrategy
             let earth = Planet(name: "Earth", isInhabited: true)
             let mars = Planet(name: "Mars", isInhabited: false)
-            try $0.insert(into: "planets")
+            try await $0.insert(into: "planets")
                 .models([earth, mars], keyEncodingStrategy: .convertToSnakeCase)
-                .run().wait()
+                .run()
         }
     }
 }
--- Sources/SQLKitBenchmark/SQLBenchmark+Planets.swift
+++ Sources/SQLKitBenchmark/SQLBenchmark+Planets.swift
@@ -2,48 +2,48 @@ import XCTest
 import SQLKit
 
 extension SQLBenchmarker {
-    public func testPlanets() throws {
-        try self.testPlanets_createSchema()
-        try self.testPlanets_seedTables()
-        try self.testPlanets_alterSchema()
+    public func testPlanets() async throws {
+        try await self.testPlanets_createSchema()
+        try await self.testPlanets_seedTables()
+        try await self.testPlanets_alterSchema()
     }
     
-    private func testPlanets_createSchema() throws {
-        try self.runTest {
-            try $0.drop(table: "planets").ifExists()
-                .run().wait()
-            try $0.drop(table: "galaxies").ifExists()
-                .run().wait()
-            try $0.create(table: "galaxies")
+    private func testPlanets_createSchema() async throws {
+        try await self.runTest {
+            try await $0.drop(table: "planets").ifExists()
+                .run()
+            try await $0.drop(table: "galaxies").ifExists()
+                .run()
+            try await $0.create(table: "galaxies")
                 .column("id",       type: .bigint, .primaryKey)
                 .column("name",     type: .text)
-                .run().wait()
-            try $0.create(table: "planets").ifNotExists()
+                .run()
+            try await $0.create(table: "planets").ifNotExists()
                 .column("id",       type: .bigint, .primaryKey)
                 .column("galaxyID", type: .bigint, .references("galaxies", "id"))
-                .run().wait()
-            try $0.alter(table: "planets")
-                .column("name",     type: .text,   .default(SQLLiteral.string("Unamed Planet")))
-                .run().wait()
-            try $0.create(index: "test_index")
+                .run()
+            try await $0.alter(table: "planets")
+                .column("name",     type: .text,   .default(SQLLiteral.string("Unnamed Planet")))
+                .run()
+            try await $0.create(index: "test_index")
                 .on("planets")
                 .column("id")
                 .unique()
-                .run().wait()
+                .run()
         }
     }
 
-    private func testPlanets_seedTables() throws {
-        try self.runTest {
+    private func testPlanets_seedTables() async throws {
+        try await self.runTest {
             // INSERT INTO "galaxies" ("id", "name") VALUES (DEFAULT, $1)
-            try $0.insert(into: "galaxies")
+            try await $0.insert(into: "galaxies")
                 .columns("id", "name")
                 .values(SQLLiteral.default, SQLBind("Milky Way"))
                 .values(SQLLiteral.default, SQLBind("Andromeda"))
                 // .value(Galaxy(name: "Milky Way"))
-                .run().wait()
+                .run()
             // SELECT * FROM galaxies WHERE name != NULL AND (name == ? OR name == ?)
-            _ = try $0.select()
+            _ = try await $0.select()
                 .column("*")
                 .from("galaxies")
                 .where("name", .notEqual, SQLLiteral.null)
@@ -51,63 +51,63 @@ extension SQLBenchmarker {
                     .where("name", .equal, SQLBind("Milky Way"))
                     .orWhere("name", .equal, SQLBind("Andromeda"))
                 }
-                .all().wait()
+                .all()
 
-            _ = try $0.select()
+            _ = try await $0.select()
                 .column("*")
                 .from("galaxies")
                 .where(SQLColumn("name"), .equal, SQLBind("Milky Way"))
                 .groupBy("id")
                 .orderBy("name", .descending)
-                .all().wait()
+                .all()
             
-            try $0.insert(into: "planets")
+            try await $0.insert(into: "planets")
                 .columns("id", "name")
                 .values(SQLLiteral.default, SQLBind("Earth"))
-                .run().wait()
+                .run()
             
-            try $0.insert(into: "planets")
+            try await $0.insert(into: "planets")
                 .columns("id", "name")
                 .values(SQLLiteral.default, SQLBind("Mercury"))
                 .values(SQLLiteral.default, SQLBind("Venus"))
                 .values(SQLLiteral.default, SQLBind("Mars"))
                 .values(SQLLiteral.default, SQLBind("Jpuiter"))
                 .values(SQLLiteral.default, SQLBind("Pluto"))
-                .run().wait()
+                .run()
 
-            try $0.select()
+            try await $0.select()
                 .column(SQLFunction("count", args: "name"))
                 .from("planets")
                 .where("galaxyID", .equal, SQLBind(5))
-                .run().wait()
+                .run()
             
-            try $0.select()
+            try await $0.select()
                 .column(SQLFunction("count", args: SQLLiteral.all))
                 .from("planets")
                 .where("galaxyID", .equal, SQLBind(5))
-                .run().wait()
+                .run()
         }
     }
     
-    private func testPlanets_alterSchema() throws {
-        try self.runTest {
+    private func testPlanets_alterSchema() async throws {
+        try await self.runTest {
             // add columns for the sake of testing adding columns
-            try $0.alter(table: "planets")
+            try await $0.alter(table: "planets")
                 .column("extra", type: .int)
-                .run().wait()
+                .run()
 
             if $0.dialect.alterTableSyntax.allowsBatch {
-                try $0.alter(table: "planets")
+                try await $0.alter(table: "planets")
                     .column("very_extra",  type: .bigint)
                     .column("extra_extra", type: .text)
-                    .run().wait()
+                    .run()
 
                 // drop, add, and modify columns
-                try $0.alter(table: "planets")
+                try await $0.alter(table: "planets")
                     .dropColumn("extra_extra")
                     .update(column: "extra", type: .text)
                     .column("hi", type: .text)
-                    .run().wait()
+                    .run()
             }
         }
     }
