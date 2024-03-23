2c52e66eca73d3e354703f4b9f5eca72eb03a257
Gwynne Raskind <gwynne@vapor.codes>
2024-03-23 09:51:28 -0500

    Improved docs for `SQLDialect` and its supporting types

--- Sources/SQLKit/Database/SQLDialect.swift
+++ Sources/SQLKit/Database/SQLDialect.swift
@@ -1,10 +1,10 @@
 /// An abstract definition of a specific dialect of SQL.
 ///
-/// SQLKit uses an ``SQLDatabase``'s dialect to control various aspects of query serialization,
-/// with the intent of keeping SQLKit's user-facing API from having to expose database-specific
-/// details as much as possible. While SQL dialects in the wild vary too widely in practice for
-/// this to ever be 100% effective, they also have enough in common to avoid having to rewrite
-/// the entire serialization logic for each database driver.
+/// SQLKit uses the dialect provided by an instance of ``SQLDatabase`` to control various aspects
+/// of query serialization, with the intent of keeping SQLKit's user-facing API from having to
+/// expose database-specific details as much as possible. While SQL dialects in the wild vary too
+/// widely in practice for this to ever be 100% effective, they also have enough in common to avoid
+/// having to rewrite every line of serialization logic for each database driver.
 public protocol SQLDialect: Sendable {
     /// The name of the dialect.
     ///
@@ -215,22 +215,27 @@ public protocol SQLDialect: Sendable {
 
 /// Encapsulates a dialect's support for `ALTER TABLE` syntax.
 public struct SQLAlterTableSyntax: Sendable {
-    /// Expression for altering a column's definition.
+    /// Expression used when altering a column's definition.
     ///
-    ///     ALTER TABLE table [alterColumnDefinitionClause] column column_definition
+    /// ```sql
+    /// ALTER TABLE table [alterColumnDefinitionClause] column column_definition
+    /// ```
     ///
     /// `nil` indicates lack of support for altering existing column definitions.
     public var alterColumnDefinitionClause: (any SQLExpression)?
 
-    /// Expression for altering a column definition's type.
+    /// Expression used when altering a column's type. Ignored if ``alterColumnDefinitionClause`` is `nil`.
     ///
-    ///     ALTER TABLE table [alterColumnDefinitionClause] column [alterColumnDefinitionTypeClause] dataType
+    /// ```sql
+    /// ALTER TABLE table [alterColumnDefinitionClause] column [alterColumnDefinitionTypeClause] dataType
+    /// ```
     ///
     /// `nil` indicates that no extra keyword is required.
     public var alterColumnDefinitionTypeKeyword: (any SQLExpression)?
 
-    /// If true, the dialect supports chaining multiple modifications together. If false,
-    /// the dialect requires separate statements for each change.
+    /// Indicates support for performing multiple alterations to a table in a single query.
+    ///
+    /// If `false`, a separate `ALTER TABLE` statement must be executed for each desired change.
     public var allowsBatch: Bool
     
     /// Memberwise initializer.
@@ -300,66 +305,90 @@ public enum SQLEnumSyntax: Sendable {
 
 /// Encapsulates a dialect's support for `CREATE TRIGGER` and `DROP TRIGGER` syntax.
 public struct SQLTriggerSyntax: Sendable {
-    /// Describes more specific support for `CREATE TRIGGER` syntax.
+    /// Describes specific feature support for `CREATE TRIGGER` syntax.
     public struct Create: OptionSet, Sendable {
         // See `RawRepresentable.rawValue`.
         public var rawValue = 0
         
         // See `OptionSet.init(rawValue:)`.
-        public init(rawValue: Int) { self.rawValue = rawValue }
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
 
-        /// `CREATE TRIGGER` statements require a `FOR EACH ROW` clause.
-        public static var requiresForEachRow: Self           { .init(rawValue: 1 << 0) }
+        /// Indicates that the `FOR EACH ROW` clause is syntactically required for trigger creation.
+        public static var requiresForEachRow: Self {
+            .init(rawValue: 1 << 0)
+        }
 
-        /// `CREATE TRIGGER` statements support specifying an implementation body inline.
-        public static var supportsBody: Self                 { .init(rawValue: 1 << 1) }
+        /// Indicates support for specifying a trigger's implementation as an inline sequence of statements.
+        public static var supportsBody: Self {
+            .init(rawValue: 1 << 1)
+        }
 
-        /// `CREATE TRIGGER` statements support a `WHEN` clause for conditionalizing execution.
-        public static var supportsCondition: Self            { .init(rawValue: 1 << 2) }
+        /// Indicates support for a conditional predicate controlling invocation of the trigger.
+        public static var supportsCondition: Self {
+            .init(rawValue: 1 << 2)
+        }
 
-        /// `CREATE TRIGGER` statements support a `DEFINER` clause for access control.
-        public static var supportsDefiner: Self              { .init(rawValue: 1 << 3) }
+        /// Indicates support for specifying a `DEFINER` clause for the purposes of access control.
+        public static var supportsDefiner: Self {
+            .init(rawValue: 1 << 3)
+        }
 
-        /// `CREATE TRIGGER` statements support both `FOR EACH ROW` and `FOR EACH STATEMENT` execution.
-        public static var supportsForEach: Self              { .init(rawValue: 1 << 4) }
+        /// Indicates support for the `FOR EACH ROW` and `FOR EACH STATEMENT` syntax.
+        public static var supportsForEach: Self {
+            .init(rawValue: 1 << 4)
+        }
 
-        /// `CREATE TRIGGER` statements support ordering relative to one another.
-        public static var supportsOrder: Self                { .init(rawValue: 1 << 5) }
+        /// `Indicates support for ordering triggers relative to one another.
+        public static var supportsOrder: Self {
+            .init(rawValue: 1 << 5)
+        }
 
-        /// `CREATE TRIGGER` statements support an `OF` clause for conditionalizing execution by columns.
-        public static var supportsUpdateColumns: Self        { .init(rawValue: 1 << 6) }
+        /// Indicates support for an `OF` clause on `UPDATE` triggers specifying that only a subset of columns should
+        /// invoke the trigger.
+        public static var supportsUpdateColumns: Self {
+            .init(rawValue: 1 << 6)
+        }
 
-        /// `CREATE TRIGGER` statements support the `CONSTRAINT` trigger type.
-        public static var supportsConstraints: Self          { .init(rawValue: 1 << 7) }
+        /// Indicates support for the `CONSTRAINT` trigger type.
+        public static var supportsConstraints: Self {
+            .init(rawValue: 1 << 7)
+        }
 
-        /// `CREATE TRIGGER` statements should perform PostgreSQL-specific syntax correctness
-        /// checks at runtime.
+        /// Indicates that PostgreSQL-specific syntax correctness checks should be made at runtime.
         ///
-        /// > Important: The checks in question are implemented as assertions, meaning they will
-        /// > trigger a fatal runtime error in debug builds, but have no effect whatsoever in
-        /// > release builds.
-        public static var postgreSQLChecks: Self             { .init(rawValue: 1 << 8) }
+        /// > Important: The checks in question are implemented as logging statements with the `.warning` level;
+        /// > invalid SQL syntax may still be generated.
+        public static var postgreSQLChecks: Self {
+            .init(rawValue: 1 << 8)
+        }
 
-        /// `CREATE TRIGGER` statements which set ``supportsCondition`` require wrapping
-        /// the condition itself in parenthesis.
-        public static var conditionRequiresParentheses: Self { .init(rawValue: 1 << 9) }
+        /// When ``supportsCondition`` is also set, indicates that the condition must be wrapped by parenthesis.
+        public static var conditionRequiresParentheses: Self {
+            .init(rawValue: 1 << 9)
+        }
     }
 
-    /// Describes more specific support for `DROP TRIGGER` syntax.
+    /// Describes specific feature support for `CREATE TRIGGER` syntax.
     public struct Drop: OptionSet, Sendable {
         // See `RawRepresentable.rawValue`.
         public var rawValue = 0
         
         // See `OptionSet.init(rawValue:)`.
-        public init(rawValue: Int) { self.rawValue = rawValue }
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
 
-        /// `DROP TRIGGER` statements support an `OF` clause defining what table the trigger
-        /// to drop is attached to.
-        public static var supportsTableName: Self            { .init(rawValue: 1 << 0) }
+        /// Indicates support for an `OF` clause indicating which table the trigger to be dropped is attached to.
+        public static var supportsTableName: Self {
+            .init(rawValue: 1 << 0)
+        }
         
-        /// `DROP TRIGGER` statements support the `CASCADE` modifier for cascading
-        /// reference deletion.
-        public static var supportsCascade: Self              { .init(rawValue: 1 << 1) }
+        /// Indicates support for the `CASCADE` modifier; see ``SQLDropBehavior`` for details.
+        public static var supportsCascade: Self {
+            .init(rawValue: 1 << 1)
+        }
     }
 
     /// Syntax options for `CREATE TRIGGER`.
@@ -375,19 +404,19 @@ public struct SQLTriggerSyntax: Sendable {
     }
 }
 
-/// The supported syntax variants which a SQL dialect can use to to specify `UPSERT` clauses.
+/// The supported syntax variants which a SQL dialect can use to to specify conflict resolution clauses.
 public enum SQLUpsertSyntax: Equatable, CaseIterable, Sendable {
-    /// Indicates usage of the SQL-standard `ON CONFLICT ...` syntax, including index and update
+    /// Indicates support for the SQL-standard `ON CONFLICT ...` syntax, including index and update
     /// predicates, the `excluded.` pseudo-table name, and the `DO NOTHING` action for "ignore
     /// conflicts".
     case standard
     
-    /// Indicates usage of the nonstandard `ON DUPLICATE KEY UPDATE ...` syntax, the `VALUES()`
+    /// Indicates support for the nonstandard `ON DUPLICATE KEY UPDATE ...` syntax, the `VALUES()`
     /// function, and `INSERT IGNORE` for "ignore conflicts". This syntax does not support
     /// conflict targets or update predicates.
     case mysqlLike
 
-    /// Indicates that upserts are not supported at all.
+    /// Indicates lack of any support for conflict resolution.
     case unsupported
 }
 
@@ -401,34 +430,49 @@ public struct SQLUnionFeatures: OptionSet, Sendable {
     public var rawValue = 0
     
     // See `OptionSet.init(rawValue:)`.
-    public init(rawValue: Int) { self.rawValue = rawValue }
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
     
-    /// Indicates basic support for `UNION` queries. All other flags are ignored unless this
-    /// one is set.
-    public static var union: Self                   { .init(rawValue: 1 << 0) }
+    /// Indicates support for `UNION DISTINCT` unions.
+    public static var union: Self {
+        .init(rawValue: 1 << 0)
+    }
     
-    /// Indicates whether the dialect supports `UNION ALL`.
-    public static var unionAll: Self                { .init(rawValue: 1 << 1) }
+    /// Indicates support for `UNION ALL` unions.
+    public static var unionAll: Self {
+        .init(rawValue: 1 << 1)
+    }
     
-    /// Indicates whether the dialect supports `INTERSECT`.
-    public static var intersect: Self               { .init(rawValue: 1 << 2) }
+    /// Indicates support for `INTERSECT DISTINCT` unions.
+    public static var intersect: Self {
+        .init(rawValue: 1 << 2)
+    }
     
-    /// Indicates whether the dialect supports `INTERSECT ALL`.
-    public static var intersectAll: Self            { .init(rawValue: 1 << 3) }
+    /// Indicates support for `INTERSECT ALL` unions.
+    public static var intersectAll: Self {
+        .init(rawValue: 1 << 3)
+    }
     
-    /// Indicates whether the dialect supports `EXCEPT`.
-    public static var except: Self                  { .init(rawValue: 1 << 4) }
+    /// Indicates support for `EXCEPT DISTINCT` unions.
+    public static var except: Self {
+        .init(rawValue: 1 << 4)
+    }
     
-    /// Indicates whether the dialect supports `EXCEPT ALL`.
-    public static var exceptAll: Self               { .init(rawValue: 1 << 5) }
+    /// Indicates support for `EXCEPT ALL` unions.
+    public static var exceptAll: Self {
+        .init(rawValue: 1 << 5)
+    }
     
-    /// Indicates whether the dialect supports explicitly specifying `DISTINCT` on
-    /// supported union types.
-    public static var explicitDistinct: Self        { .init(rawValue: 1 << 6) }
+    /// Indicates that the `DISTINCT` modifier must be explicitly specified for the relevant union types.
+    public static var explicitDistinct: Self {
+        .init(rawValue: 1 << 6)
+    }
     
-    /// Indicates whether the dialect allows parenthesizing the individual `SELECT` queries
-    /// in a union.
-    public static var parenthesizedSubqueries: Self { .init(rawValue: 1 << 7) }
+    /// Indicates that the individual `SELECT` queries in a union must be parenthesized.
+    public static var parenthesizedSubqueries: Self {
+        .init(rawValue: 1 << 7)
+    }
 }
 
 /// Provides defaults for many of the ``SQLDialect`` properties. The defaults are chosen to
@@ -436,81 +480,97 @@ public struct SQLUnionFeatures: OptionSet, Sendable {
 /// as possible, so as to avoid breaking all existing dialects every time a new requirement
 /// is added to the protocol and allow gradual adoption of new capabilities.
 extension SQLDialect {
+    /// Default implementation of ``literalStringQuote-3ur0m``.
     @inlinable
     public var literalStringQuote: any SQLExpression {
         SQLRaw("'")
     }
 
+    /// Default implementation of ``autoIncrementFunction-1ktxy``.
     @inlinable
     public var autoIncrementFunction: (any SQLExpression)? {
         nil
     }
 
+    /// Default implementation of ``literalDefault-7nz7t``.
     @inlinable
     public var literalDefault: any SQLExpression {
         SQLRaw("DEFAULT")
     }
 
+    /// Default implementation of ``supportsIfExists-5dxcu``.
     @inlinable
     public var supportsIfExists: Bool {
         true
     }
 
+    /// Default implementation of ``enumSyntax-7atad``.
     @inlinable
     public var enumSyntax: SQLEnumSyntax {
         .unsupported
     }
     
+    /// Default implementation of ``supportsDropBehavior-6vvl0``.
     @inlinable
     public var supportsDropBehavior: Bool {
         false
     }
 
+    /// Default implementation of ``supportsReturning-r61k``.
     @inlinable
     public var supportsReturning: Bool {
         false
     }
 
+    /// Default implementation of ``triggerSyntax-9579a``.
     @inlinable
     public var triggerSyntax: SQLTriggerSyntax {
         .init()
     }
 
+    /// Default implementation of ``alterTableSyntax-9bmcr``.
     @inlinable
     public var alterTableSyntax: SQLAlterTableSyntax {
         .init()
     }
 
+    /// Default implementation of ``customDataType(for:)-2firt``.
     @inlinable
     public func customDataType(for: SQLDataType) -> (any SQLExpression)? {
         nil
     }
 
+    /// Default implementation of ``normalizeSQLConstraint(identifier:)-3vca6``.
     @inlinable
     public func normalizeSQLConstraint(identifier: any SQLExpression) -> any SQLExpression {
         identifier
     }
 
+    /// Default implementation of ``upsertSyntax-snn6``.
     @inlinable
     public var upsertSyntax: SQLUpsertSyntax {
         .unsupported
     }
 
+    /// Default implementation of ``unionFeatures-473tk``.
     @inlinable
     public var unionFeatures: SQLUnionFeatures {
         [.union, .unionAll]
     }
 
+    /// Default implementation of ``sharedSelectLockExpression-6lb8t``.
     @inlinable
     public var sharedSelectLockExpression: (any SQLExpression)? {
         nil
     }
 
+    /// Default implementation of ``exclusiveSelectLockExpression-21gkt``.
     @inlinable
     public var exclusiveSelectLockExpression: (any SQLExpression)? {
         nil
     }
 
+    /// Default implementation of ``nestedSubpathExpression(in:for:)-7d4cw``.
     @inlinable
     public func nestedSubpathExpression(in: any SQLExpression, for: [String]) -> (any SQLExpression)? {
         nil
