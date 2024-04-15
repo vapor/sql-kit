cd0c5b7b4b61d6416f85964763f61c8967d093a5
Gwynne Raskind <gwynne@vapor.codes>
2024-04-15 05:34:04 -0500

    Revert the changed package name in the exports (#174)
    * Revert the changed package name in the exports, this apparently breaks building against FluentKit when not using any of the driver packages. Add CI to test this scenario.

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -49,6 +49,7 @@ let package = Package(
 )
 
 var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
     .enableUpcomingFeature("ConciseMagicFile"),
     .enableUpcomingFeature("ForwardTrailingClosures"),
     .enableUpcomingFeature("ImportObjcForwardDeclarations"),
--- Sources/SQLKit/Exports.swift
+++ Sources/SQLKit/Exports.swift
@@ -1,3 +1,3 @@
-@_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoop
-@_documentation(visibility: internal) @_exported import class NIOCore.EventLoopFuture
+@_documentation(visibility: internal) @_exported import protocol NIO.EventLoop
+@_documentation(visibility: internal) @_exported import class NIO.EventLoopFuture
 @_documentation(visibility: internal) @_exported import struct Logging.Logger
