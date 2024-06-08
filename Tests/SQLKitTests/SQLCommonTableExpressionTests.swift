db4789b161aacfd823786085623602d18bd6041e
Gwynne Raskind <gwynne@vapor.codes>
2024-06-07 21:32:24 -0500

    Add a couple of real-world CTE queries to tests

--- Tests/SQLKitTests/SQLCommonTableExpressionTests.swift
+++ Tests/SQLKitTests/SQLCommonTableExpressionTests.swift
@@ -240,4 +240,64 @@ final class SQLCommonTableExpressionTests: XCTestCase {
         
         XCTAssertSerialization(of: self.db.raw("\(query)"), is: "WITH ``x`` AS (VALUES(``1``)), ``x`` AS (VALUES(``1``)), (FOO) SELECT ``a``.``b``")
     }
+    
+    func testMoreRealisticCTEs() {
+        // Simple sub-SELECT avoidance
+        // Taken from https://www.postgresql.org/docs/16/queries-with.html#QUERIES-WITH-SELECT
+        self.db._dialect.identifierQuote = SQLRaw("\"")
+        XCTAssertSerialization(
+            of: self.db.select()
+                .with("regional_sales", as: SQLSubquery.select { $0
+                    .column("region").column(SQLFunction("SUM", args: SQLColumn("amount")), as: "total_sales").from("orders").groupBy("region")
+                })
+                .with("top_regions", as: SQLSubquery.select { $0
+                    .column("region").from("regional_sales")
+                    .where("total_sales", .greaterThan, SQLSubquery.select { $0
+                        .column(SQLBinaryExpression(SQLFunction("SUM", args: SQLIdentifier("total_sales")), .divide, SQLLiteral.numeric("10")))
+                        .from("regional_sales")
+                    })
+                })
+                .columns("region", "product")
+                .column(SQLFunction("SUM", args: SQLColumn("quantity")), as: "product_units")
+                .column(SQLFunction("SUM", args: SQLColumn("amount")), as: "product_sales")
+                .from("orders").where("region", .in, SQLSubquery.select { $0.column("region").from("top_regions") }).groupBy("region").groupBy("product"),
+            is: """
+                WITH
+                "regional_sales" AS (SELECT "region", SUM("amount") AS "total_sales" FROM "orders" GROUP BY "region"),
+                "top_regions" AS (SELECT "region" FROM "regional_sales" WHERE "total_sales" > (SELECT SUM("total_sales") / 10 FROM "regional_sales"))
+                SELECT "region", "product", SUM("quantity") AS "product_units", SUM("amount") AS "product_sales"
+                FROM "orders"
+                WHERE "region" IN (SELECT "region" FROM "top_regions")
+                GROUP BY "region", "product"
+                """.replacing("\n", with: " ")
+        )
+        
+        // Fibonacci series generator
+        // Taken from https://dev.mysql.com/doc/refman/8.4/en/with.html#common-table-expressions-recursive-fibonacci-series
+        self.db._dialect.identifierQuote = SQLRaw("`")
+        self.db._dialect.unionFeatures = [.union, .unionAll]
+        XCTAssertSerialization(
+            of: self.db.select()
+                .with(recursive: "fibonacci", columns: ["n", "fib_n", "next_fib_n"], as: SQLSubquery.union { $0
+                    .columns(SQLLiteral.numeric("1"), SQLLiteral.numeric("0"), SQLLiteral.numeric("1"))
+                }.union(all: { $0
+                    .column(SQLBinaryExpression(SQLColumn("n"), .add, SQLLiteral.numeric("1")))
+                    .column("next_fib_n")
+                    .column(SQLBinaryExpression(SQLColumn("fib_n"), .add, SQLColumn("next_fib_n")))
+                    .from("fibonacci")
+                    .where("n", .lessThan, SQLLiteral.numeric("10"))
+                })
+                .finish())
+                .column(SQLLiteral.all)
+                .from("fibonacci"),
+            is: """
+                WITH RECURSIVE `fibonacci` (`n`, `fib_n`, `next_fib_n`) AS (
+                    SELECT 1, 0, 1
+                    UNION ALL
+                    SELECT `n` + 1, `next_fib_n`, `fib_n` + `next_fib_n` FROM `fibonacci` WHERE `n` < 10
+                )
+                SELECT * FROM `fibonacci`
+                """.replacing("    ", with: "").replacing("(\n", with: "(").replacing("\n)", with: ")").replacing("\n", with: " ")
+        )
+    }
 }
