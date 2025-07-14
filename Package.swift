f0c7fa6a9776def6db1c6d8a5490138f7d2389b1
Scott Marchant <scottm@passivelogic.com>
2025-07-14 11:15:40 -0600

    refactor: Only elide the full NIO dependency if WASI is the target platform. Otherwise, leave exports and dependencies unchanged to minimize any disruption to downstream dependencies.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,21 @@
 // swift-tools-version:5.10
 import PackageDescription
 
+let nonWASIPlatforms: [Platform] = [
+    .macOS,
+    .macCatalyst,
+    .iOS,
+    .tvOS,
+    .watchOS,
+    .visionOS,
+    .driverKit,
+    .linux,
+    .windows,
+    .android,
+    // .wasi, // Everything but WASI
+    .openbsd,
+]
+
 let package = Package(
     name: "sql-kit",
     platforms: [
@@ -28,6 +43,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Collections", package: "swift-collections"),
                 .product(name: "Logging", package: "swift-log"),
+                .product(name: "NIO", package: "swift-nio", condition: .when(platforms: nonWASIPlatforms)),
                 .product(name: "NIOCore", package: "swift-nio"),
             ],
             swiftSettings: swiftSettings
--- Sources/SQLKit/Exports.swift
+++ Sources/SQLKit/Exports.swift
@@ -1,3 +1,8 @@
+#if os(WASI)
 @_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoop
 @_documentation(visibility: internal) @_exported import class NIOCore.EventLoopFuture
+#else
+@_documentation(visibility: internal) @_exported import protocol NIO.EventLoop
+@_documentation(visibility: internal) @_exported import class NIO.EventLoopFuture
+#endif
 @_documentation(visibility: internal) @_exported import struct Logging.Logger
